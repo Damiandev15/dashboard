4ec18d94d612eb2a98340a562837f37f99523e01
Damiandev15 <damian@veflat.com>
2024-03-20 12:03:07 -0400

    dasboard

---
+++ src/components/Card.tsx
@@ -0,0 +1,20 @@
+import { Card, Metric, Text } from "@tremor/react";
+
+export function CardUsageExample() {
+  return (
+    <div>
+      <Card
+        className="mx-auto max-w-xs"
+        decoration="top"
+        decorationColor="indigo"
+      >
+        <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+          Sales
+        </p>
+        <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
+          $34,743
+        </p>
+      </Card>
+    </div>
+  );
+}
